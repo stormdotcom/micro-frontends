0242ab122f785483877be89fb13c927598f51995
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-08-14 04:47:54 +0530

    added

--- landingPage/src/components/Project/Footer.js
+++ landingPage/src/components/Project/Footer.js
@@ -5,26 +5,26 @@ const Footer = ({ MiddleFooterComponent }) => {
     return (
         <>
             {MiddleFooterComponent && <MiddleFooterComponent />}
-            <footer className="bg-gray-100 py-6 border border-top-1">
-                <div className="container mx-auto flex justify-between items-center">
+            <footer className="bg-gray-100 py-4 border-t">
+                <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
 
-                    <div className="text-gray-700 text-sm">
+                    <div className="text-gray-700 text-sm text-center md:text-left">
                         © Hotspot Mobile – All Rights Reserved.
                     </div>
 
                     <div className="flex justify-center">
-                        <Image alt="logo" src="/assets/images/hm_logo.png" width={30} height={30} />
+                        <Image alt="logo" src="/assets/images/hm_logo.png" width={24} height={24} />
                     </div>
 
-
-                    <div className='text-gray-700 text-sm'>
-                        Made with ♥ by
-                        <a href='https://ajmalnasumudeen.in/' target='_blank'> Ajmal Nasumudeen</a>
+                    <div className="text-gray-700 text-sm text-center md:text-right">
+                        Made with ♥ by{' '}
+                        <a href='https://ajmalnasumudeen.in/' target='_blank' className="underline hover:text-primary">
+                            Ajmal Nasumudeen
+                        </a>
                     </div>
                 </div>
             </footer>
         </>
-
     )
 }
 
