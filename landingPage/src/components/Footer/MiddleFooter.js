acaa2a1876e3568843153ec525528dddec05ef11
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-08-14 04:32:34 +0530

    added footer px-2

--- landingPage/src/components/Footer/MiddleFooter.js
+++ landingPage/src/components/Footer/MiddleFooter.js
@@ -43,7 +43,7 @@ const MiddleFooter = () => {
     ];
 
     return (
-        <footer className="bg-gray-100 py-10">
+        <footer className="bg-gray-100 py-10 px-2">
             <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
 
                 {/* Popular Categories */}
